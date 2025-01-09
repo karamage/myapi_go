4f5cd7a5ae34a22256206eaa2ac135be22436cab
karamage <karamage.umauma@gmail.com>
2025-01-09 16:40:25 +0900

    feat: implement JSON response for PostArticleHandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"myapi/models"
 	"net/http"
 	"strconv"
 
@@ -14,7 +16,14 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonData)
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
