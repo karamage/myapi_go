77a6a20c5b0c0ef97d481ab02bf09326504a0497
karamage <karamage.umauma@gmail.com>
2025-01-11 14:55:10 +0900

    feat: add MySQL driver and database connection setup in main.go; update go.mod and go.sum for new dependencies

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
-	"log"
-	"myapi/handlers"
-	"net/http"
+	/*
+		"log"
+		"myapi/handlers"
+		"net/http"
+		"github.com/gorilla/mux"
+	*/
+	"database/sql"
+	"fmt"
 
-	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := mux.NewRouter()
+	dbUser := "root"
+	dbPassword := "pass"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@/%s", dbUser, dbPassword, dbDatabase)
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-	log.Print("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	if err := db.Ping(); err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("Successfully connected to the database")
+	}
+
+	/*
+		r := mux.NewRouter()
+
+		r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+		r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+		r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+		r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+		r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+		r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+		log.Print("server start at port 8080")
+		log.Fatal(http.ListenAndServe(":8080", r))
+	*/
 }
