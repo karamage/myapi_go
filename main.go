dba2511e3994bb5580d967252bf3934ab57f4e2d
karamage <karamage.umauma@gmail.com>
2025-01-13 16:05:41 +0900

    refactor: clean up main.go by removing commented-out code and organizing database connection logic

--- main.go
+++ main.go
@@ -1,74 +1,73 @@
 package main
 
 import (
-	/*
-		"log"
-		"myapi/handlers"
-		"net/http"
-		"github.com/gorilla/mux"
-	*/
-	"database/sql"
-	"fmt"
-	"myapi/models"
+	"log"
+	"myapi/handlers"
+	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
-)
+	"github.com/gorilla/mux"
+	/*
+		"database/sql"
+		"fmt"
+		"myapi/models"
+		_ "github.com/go-sql-driver/mysql"
+	*/)
 
 func main() {
-	dbUser := "sample"
-	dbPassword := "pass"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	/*
+		dbUser := "sample"
+		dbPassword := "pass"
+		dbDatabase := "sampledb"
+		dbConn := fmt.Sprintf("%s:%s@/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	const sqlStr = `
-		select *
-		from articles;
-	`
-	rows, err := db.Query(sqlStr)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
-
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		var article models.Article
-		var createdTime sql.NullTime
-
-		err := rows.Scan(&article.ID, &article.Title, &article.Contents,
-			&article.UserName, &article.NiceNum, &createdTime)
-
-		if createdTime.Valid {
-			article.CreatedAt = createdTime.Time
+		db, err := sql.Open("mysql", dbConn)
+		if err != nil {
+			panic(err.Error())
 		}
+		defer db.Close()
 
+		const sqlStr = `
+			select *
+			from articles;
+		`
+		rows, err := db.Query(sqlStr)
 		if err != nil {
 			fmt.Println(err)
-		} else {
-			articleArray = append(articleArray, article)
+			return
 		}
-	}
+		defer rows.Close()
 
-	fmt.Printf("%+v\n", articleArray)
+		articleArray := make([]models.Article, 0)
+		for rows.Next() {
+			var article models.Article
+			var createdTime sql.NullTime
 
-	/*
-		r := mux.NewRouter()
+			err := rows.Scan(&article.ID, &article.Title, &article.Contents,
+				&article.UserName, &article.NiceNum, &createdTime)
 
-		r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-		r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-		r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-		r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-		r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-		r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+			if createdTime.Valid {
+				article.CreatedAt = createdTime.Time
+			}
 
-		log.Print("server start at port 8080")
-		log.Fatal(http.ListenAndServe(":8080", r))
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				articleArray = append(articleArray, article)
+			}
+		}
+
+		fmt.Printf("%+v\n", articleArray)
 	*/
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+	log.Print("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
