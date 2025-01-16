349693bb2bb84feb8c52293a7563a0861f4b3e9d
karamage <karamage.umauma@gmail.com>
2025-01-16 16:15:21 +0900

    feat: add SelectArticleList function to retrieve paginated list of articles from the database

--- repositories/articles.go
+++ repositories/articles.go
@@ -5,6 +5,10 @@ import (
 	"myapi/models"
 )
 
+const (
+	articleNumPerPage = 5
+)
+
 // 新規投稿をデータベースに insert する関数
 func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	const sqlStr = `
@@ -25,4 +29,30 @@ func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	newArticle.ID = int(id)
 
 	return newArticle, nil
+}
+
+// 変数 page で指定されたページの記事一覧を取得する関数
+// -> 取得した記事一覧と、発生したエラーを返す
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+		select article_id, title, contents, username, nice
+		from articles
+		limit ? offset ?;
+	`
+
+	rows, err := db.Query(sqlStr, articleNumPerPage, ((page - 1) * articleNumPerPage))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		articleArray = append(articleArray, article)
+	}
+
+	return articleArray, nil
 }
