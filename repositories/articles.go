9b5eab9a05ec352a9976f0f46b33a7d1417d2d8f
karamage <karamage.umauma@gmail.com>
2025-01-16 15:30:52 +0900

    feat: add InsertArticle function to handle article submissions in the database

---
+++ repositories/articles.go
@@ -0,0 +1,28 @@
+package repositories
+
+import (
+	"database/sql"
+	"myapi/models"
+)
+
+// 新規投稿をデータベースに insert する関数
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+	insert into articles (title, contents, username, nice, created_at) values
+	(?, ?, ?, 0, now());
+	`
+
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, _ := result.LastInsertId()
+
+	newArticle.ID = int(id)
+
+	return newArticle, nil
+}
