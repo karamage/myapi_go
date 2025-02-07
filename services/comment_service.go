586bc3adfee3b9428e2c0cf0c4b4139f7f5326f8
karamage <karamage.umauma@gmail.com>
2025-02-07 16:56:36 +0900

    refactor: update PostCommentService to use MyAppService's database connection

--- services/comment_service.go
+++ services/comment_service.go
@@ -5,15 +5,9 @@ import (
 	"myapi/repositories"
 )
 
-func PostCommentService(comment models.Comment) (models.Comment, error) {
-	db, err := connectDB()
-	if err != nil {
-		return models.Comment{}, err
-	}
-	defer db.Close()
-
+func (s *MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	// repositories 層の InsertComment 関数を呼び出し、新しいコメントをデータベースに保存する
-	newComment, err := repositories.InsertComment(db, comment)
+	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
 		return models.Comment{}, err
 	}
