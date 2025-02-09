57c17122efcd4b8759cd9fd994bd03a7808144d6
karamage <karamage.umauma@gmail.com>
2025-02-09 14:16:27 +0900

    refactor: update handlers package to use MyAppController methods

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -19,11 +19,11 @@ func NewMyAppController(service *services.MyAppService) *MyAppController {
 	return &MyAppController{service: service}
 }
 
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 
-func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
@@ -31,7 +31,7 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	newArticle, err := services.PostArticleService(reqArticle)
+	newArticle, err := c.service.PostArticleService(reqArticle)
 	if err != nil {
 		http.Error(w, "Failed to post article", http.StatusInternalServerError)
 		return
@@ -40,7 +40,7 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(newArticle)
 }
 
-func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
 
 	var page int
@@ -55,7 +55,7 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 		page = 1
 	}
 
-	articleList, err := services.GetArticleListService(page)
+	articleList, err := c.service.GetArticleListService(page)
 	if err != nil {
 		http.Error(w, "Failed to get article list", http.StatusInternalServerError)
 		return
@@ -64,14 +64,14 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(articleList)
 }
 
-func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
 		http.Error(w, "Invalid article ID", http.StatusBadRequest)
 		return
 	}
 
-	article, err := services.GetArticleService(articleID)
+	article, err := c.service.GetArticleService(articleID)
 	if err != nil {
 		http.Error(w, "Failed to get article", http.StatusInternalServerError)
 		return
@@ -80,14 +80,14 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(article)
 }
 
-func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
 		http.Error(w, "Invalid article ID", http.StatusBadRequest)
 		return
 	}
 
-	article, err := services.PostNiceService(articleID)
+	article, err := c.service.PostNiceService(articleID)
 	if err != nil {
 		http.Error(w, "Failed to post nice", http.StatusInternalServerError)
 		return
@@ -96,14 +96,14 @@ func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(article)
 }
 
-func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	var comment models.Comment
 	if err := json.NewDecoder(req.Body).Decode(&comment); err != nil {
 		http.Error(w, "Invalid comment data", http.StatusBadRequest)
 		return
 	}
 
-	newComment, err := services.PostCommentService(comment)
+	newComment, err := c.service.PostCommentService(comment)
 	if err != nil {
 		http.Error(w, "Failed to post comment", http.StatusInternalServerError)
 		return
