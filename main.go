1d7045e43977b38777b9b2a6e200f81f6c9d127b
karamage <karamage.umauma@gmail.com>
2025-02-11 13:38:20 +0900

    refactor: move router setup to a new routers package

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"log"
 	"myapi/controllers"
+	"myapi/routers"
 	"myapi/services"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -18,14 +17,7 @@ func main() {
 	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+	r := routers.NewRouter(con)
 
 	log.Print("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
